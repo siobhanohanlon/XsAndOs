29c3ec2f9970e7c897753976e7f084bed0a4b478
Siobhán O'Hanlon <siobhanhanlon@outlook.com>
2022-01-10 02:56:57 +0000

    New Game Complete- Board Set up

--- XsAndOs/Source.c
+++ XsAndOs/Source.c
@@ -1,16 +1,35 @@
 #include <stdio.h>
 
 //Declare Variables
-char squares[10] = { '0','1','2','3','4','5','6','7','8','9' };
+char squares[10] = {'0','1','2','3','4','5','6','7','8','9'};
 
 //Define Functions
 void NewGame();
+void ContinueGame();
+void PlayGame();
+void SaveGame();
+int CheckWin();
 
 void main()
 {
-	printf("\t\tWelcome to Xs & Os");
+	int gameChoice;
 
-	NewGame();
+	printf("\t\t Welcome to Xs & Os\n\n");
+	printf("\tPlayer 1: X\t\tPlayer 2: O\n\n");
+
+	printf("\nPlease Enter 1 to Load a Saved Game or any Other Number to Start a New Game:\t");
+	scanf("%d", &gameChoice);
+
+	if (gameChoice == 1)
+	{
+		//Go to Load Game Function
+		ContinueGame();
+	}
+	else
+	{
+		//Start a New Game
+		NewGame();
+	}
 }
 
 //NewGame
@@ -18,7 +37,22 @@ void NewGame()
 {
 	//Declare Variables
 	int player = 1;
-	int sqChoice, i;
 
-	printf("");
+	//Create the Board
+	printf("\t\t    |         |\n");
+	printf("\t       %c    |    %c    |    %c\n", squares[0], squares[1], squares[2]);
+	printf("\t   _________|_________|_________\n");
+	printf("\t\t    |         |\n");
+	printf("\t       %c    |    %c    |    %c\n", squares[4], squares[5], squares[6]);
+	printf("\t   _________|_________|_________\n");
+	printf("\t\t    |         |\n");
+	printf("\t       %c    |    %c    |    %c\n", squares[7], squares[8], squares[9]);
+	printf("\t\t    |         |\n");
+
+	PlayGame(player);
 }
+
+void PlayGame();
+
+//Check if Game is Won
+CheckWin();
