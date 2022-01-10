9bccad3ccba05d488d630bbfc1c3d69104ad79ce
Siobhán O'Hanlon <siobhanhanlon@outlook.com>
2022-01-10 02:12:16 +0000

    Add project files.

---
+++ XsAndOs/Source.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+//Declare Variables
+char squares[10] = { '0','1','2','3','4','5','6','7','8','9' };
+
+//Define Functions
+void NewGame();
+
+void main()
+{
+	printf("\t\tWelcome to Xs & Os");
+
+	NewGame();
+}
+
+//NewGame
+void NewGame()
+{
+	//Declare Variables
+	int player = 1;
+	int sqChoice, i;
+
+	printf("");
+}
